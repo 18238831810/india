0fcd4abfc2684a14490a2895f09551aefb04c9ca
Administrator <d>
2021-06-06 19:16:15 +0800

    @ApiParam(hidden = true)

--- crs-admin/src/main/java/com/cf/crs/entity/OrderEntity.java
+++ crs-admin/src/main/java/com/cf/crs/entity/OrderEntity.java
@@ -2,6 +2,9 @@ package com.cf.crs.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +20,12 @@ public class OrderEntity {
     /**
      * 创建时间
      */
+    @ApiParam(hidden = true)
     private long ctime;
     /**
      * 撮合时间
      */
+    @ApiParam(hidden = true)
     private long utime;
     /**
      * 直播房间号
@@ -43,6 +48,7 @@ public class OrderEntity {
     /**
      * 上期价格
      */
+    @ApiParam(hidden = true)
     private String earlyStagePrice;
     /**
      * 上期时间
@@ -51,31 +57,37 @@ public class OrderEntity {
     /**
      * 下期价格
      */
+    @ApiParam(hidden = true)
     private String nextStagePrice;
     /**
      * 下期时间
      */
+    @ApiParam(hidden = true)
     private long nextStageTime;
 
     /**
      * 杠杆倍数
      */
+    @ApiParam(hidden = true)
     private double lever;
 
     /**
      * 盈利
      */
+    @ApiParam(hidden = true)
     private double profit;
 
     /**
      * 行情周期
      */
+    @ApiParam(hidden = true)
     private String marketCycle;
 
     /**
      * 状态
      * 0:刚生成，1是处理 -1：为撤销订单
      */
+    @ApiParam(hidden = true)
     private int status;
 
     @TableField(exist = false)
