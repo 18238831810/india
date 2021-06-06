111cc195ff999abd7ec2dc56bb2bf107298f3c80
Administrator <d>
2021-06-06 18:21:36 +0800

    优化

--- crs-admin/src/main/java/com/cf/crs/controller/OrderController.java
+++ crs-admin/src/main/java/com/cf/crs/controller/OrderController.java
@@ -25,7 +25,7 @@ import java.util.List;
 @Api(tags = "订单操作")
 @RestController
 @RequestMapping("/order")
-public class OrderController extends  BaseController{
+public class OrderController extends BaseController {
 
     @Autowired
     OrderServiceImpl orderService;
@@ -39,7 +39,7 @@ public class OrderController extends  BaseController{
             @ApiImplicitParam(name = "roomId", value = "房间号ID", required = true, dataType = "String"),
             @ApiImplicitParam(name = "token", value = "下单人的token", required = true, dataType = "String"),
             @ApiImplicitParam(name = "payment", value = "下单金额", required = true, dataType = "double"),
-            @ApiImplicitParam(name = "buyDirection",allowableValues = "rise,fall,equal",value = "下单方向", required = true, dataType = "String")
+            @ApiImplicitParam(name = "buyDirection", allowableValues = "rise,fall,equal", value = "下单方向", required = true, dataType = "String")
     })
     public Result saveOrder(OrderEntity orderEntity) {
         orderEntity.setUid(getUidFromToken(orderEntity.getToken()));
@@ -63,35 +63,33 @@ public class OrderController extends  BaseController{
     @ApiOperation("订单列表")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "token", value = "下单人的token", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "pageNum", value = "页码", required = true, dataType = "int" ,defaultValue = "1"),
-            @ApiImplicitParam(name = "pageSize", value = "每页显示多少条", required = true, dataType = "int" ,defaultValue = "15")
+            @ApiImplicitParam(name = "pageNum", value = "页码", required = true, dataType = "int", defaultValue = "1"),
+            @ApiImplicitParam(name = "pageSize", value = "每页显示多少条", required = true, dataType = "int", defaultValue = "15")
     })
     public Result listOrder(String token, QueryPage queryPage) {
-        PagingBase<OrderEntity> pagingBase = orderService.listOrder(getUidFromToken(token),queryPage);
+        PagingBase<OrderEntity> pagingBase = orderService.listOrder(getUidFromToken(token), queryPage);
         return new Result<PagingBase>().ok(pagingBase);
     }
 
     @PostMapping("/saveCommission")
     @ApiOperation("生成直播提成")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "day", value = "生成指定多少天前的一天数据", required = true, dataType = "Integer",defaultValue = "null"),
+            @ApiImplicitParam(name = "day", value = "生成指定多少天前的一天数据", required = true, dataType = "Integer", defaultValue = "null"),
     })
-    public Result saveCommission(Integer day)
-    {
-       int total= orderCommissionService.saveOrderCommission(day);
-       return new Result<>().error(200, "总条数:"+total);
+    public Result saveCommission(Integer day) {
+        int total = orderCommissionService.saveOrderCommission(day);
+        return new Result<>().error(200, "总条数:" + total);
     }
 
     @GetMapping("/candlestick")
     @ApiOperation("行情数据")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "symbol", value = "交易对", required = true, dataType = "String",defaultValue = "btcusdt"),
-            @ApiImplicitParam(name = "interval",  allowableValues ="1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d", value = "行情周期", required = true, dataType = "String",defaultValue = "1m"),
-            @ApiImplicitParam(name = "size",   value = "行情条数", dataType = "int",defaultValue = "240"),
+            @ApiImplicitParam(name = "symbol", value = "交易对", required = true, dataType = "String", defaultValue = "btcusdt"),
+            @ApiImplicitParam(name = "interval", allowableValues = "1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d", value = "行情周期", required = true, dataType = "String", defaultValue = "1m"),
+            @ApiImplicitParam(name = "size", value = "行情条数", dataType = "int", defaultValue = "240"),
     })
-        public Result getCandlestick(String symbol ,String interval,Integer size)
-    {
-        List<Candlestick> result= orderService.getCandlestickList(StringUtil.isBlank(symbol)?"btcusdt":symbol,StringUtil.isBlank(interval)?"1m":interval,size==null?240:size);
-        return new Result<>().ok(result);
+    public Result<List<Candlestick>> getCandlestick(String symbol, String interval, Integer size) {
+        List<Candlestick> result = orderService.getCandlestickList(StringUtil.isBlank(symbol) ? "btcusdt" : symbol, StringUtil.isBlank(interval) ? "1m" : interval, size == null ? 240 : size);
+        return new Result<List<Candlestick>>().ok(result);
     }
 }
