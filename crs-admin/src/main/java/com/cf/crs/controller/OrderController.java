17fb8a1d5e8ae2f183c0b8f8dc358ed498d50e3b
Administrator <d>
2021-06-06 12:35:48 +0800

    swag文档修改

--- crs-admin/src/main/java/com/cf/crs/controller/OrderController.java
+++ crs-admin/src/main/java/com/cf/crs/controller/OrderController.java
@@ -37,7 +37,7 @@ public class OrderController extends  BaseController{
             @ApiImplicitParam(name = "roomId", value = "房间号ID", required = true, dataType = "String"),
             @ApiImplicitParam(name = "token", value = "下单人的token", required = true, dataType = "String"),
             @ApiImplicitParam(name = "payment", value = "下单金额", required = true, dataType = "double"),
-            @ApiImplicitParam(name = "buyDirection", value = "下单方向(rise/fall/equal)", required = true, dataType = "String")
+            @ApiImplicitParam(name = "buyDirection",allowableValues = "rise,fall,equal",value = "下单方向", required = true, dataType = "String")
     })
     public Result saveOrder(OrderEntity orderEntity) {
         orderEntity.setUid(getUidFromToken(orderEntity.getToken()));
@@ -84,7 +84,7 @@ public class OrderController extends  BaseController{
     @ApiOperation("行情数据")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "symbol", value = "交易对", required = true, dataType = "String",defaultValue = "btcusdt"),
-            @ApiImplicitParam(name = "interval", value = "行情周期(1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d)", required = true, dataType = "String",defaultValue = "1d"),
+            @ApiImplicitParam(name = "interval",  allowableValues ="1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d", value = "行情周期", required = true, dataType = "String",defaultValue = "1d"),
     })
     public Result getCandlestick(String symbol ,String interval)
     {
