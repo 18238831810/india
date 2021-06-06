97b985439266270bedabdd257cff8a372e28f501
Administrator <d>
2021-06-06 19:41:45 +0800

    添加token从header中获取的实现

--- crs-admin/src/main/java/com/cf/crs/controller/BaseController.java
+++ crs-admin/src/main/java/com/cf/crs/controller/BaseController.java
@@ -1,9 +1,30 @@
 package com.cf.crs.controller;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
 public abstract class BaseController {
 
-    public long getUidFromToken(String token)
+    public long getUid()
     {
+        String token =getTokenFromHeader();
         return 0l;
+    }
+
+    public String  getTokenFromHeader()
+    {
+        return fetchCurrentRequest().getHeader("token");
+    }
+
+    /**
+     * 返回当前的request
+     *
+     * @return
+     */
+    public HttpServletRequest fetchCurrentRequest() {
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return sra.getRequest();
     }
 }
--- crs-admin/src/main/java/com/cf/crs/controller/OrderController.java
+++ crs-admin/src/main/java/com/cf/crs/controller/OrderController.java
@@ -1,8 +1,6 @@
 package com.cf.crs.controller;
 
-import com.binance.api.client.CandlesticksCache;
 import com.binance.api.client.constant.OrderErrorEnum;
-import com.binance.api.client.domain.market.Candlestick;
 import com.cf.crs.common.entity.PagingBase;
 import com.cf.crs.common.entity.QueryPage;
 import com.cf.crs.common.utils.Result;
@@ -16,7 +14,6 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.jsoup.helper.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,12 +36,11 @@ public class OrderController extends BaseController {
     @ApiOperation("保存订单")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "roomId", value = "房间号ID", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "token", value = "下单人的token", required = true, dataType = "String"),
             @ApiImplicitParam(name = "payment", value = "下单金额", required = true, dataType = "double"),
             @ApiImplicitParam(name = "buyDirection", allowableValues = "rise,fall,equal", value = "下单方向", required = true, dataType = "String")
     })
     public Result saveOrder(OrderEntity orderEntity) {
-        orderEntity.setUid(getUidFromToken(orderEntity.getToken()));
+        orderEntity.setUid(getUid());
         OrderErrorEnum orderErrorEnum = orderService.saveUserOrder(orderEntity);
         return orderErrorEnum == null ? new Result<>() : new Result<>().error(orderErrorEnum.getCode(), orderErrorEnum.getError());
     }
@@ -53,11 +49,10 @@ public class OrderController extends BaseController {
     @ApiOperation("撤单订单")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", value = "订单ID", required = true, dataType = "long"),
-            @ApiImplicitParam(name = "token", value = "下单人的token", required = true, dataType = "String")
     })
-    public Result delOrder(int id,String token) {
+    public Result delOrder(int id) {
         OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setUid(getUidFromToken(token));
+        orderEntity.setUid(getUid());
         orderEntity.setId(id);
         OrderErrorEnum orderErrorEnum = orderService.updateOrderToDelStatus(orderEntity);
         return orderErrorEnum == null ? new Result<>() : new Result<>().error(orderErrorEnum.getCode(), orderErrorEnum.getError());
@@ -66,19 +61,18 @@ public class OrderController extends BaseController {
     @GetMapping("/list")
     @ApiOperation("订单列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "token", value = "下单人的token", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "pageNum", value = "页码,默认1",  dataType = "int", defaultValue = "1"),
+            @ApiImplicitParam(name = "pageNum", value = "页码,默认1", dataType = "int", defaultValue = "1"),
             @ApiImplicitParam(name = "pageSize", value = "每页显示多少条,默认15", dataType = "int", defaultValue = "15")
     })
     public Result listOrder(String token, QueryPage queryPage) {
-        PagingBase<OrderEntity> pagingBase = orderService.listOrder(getUidFromToken(token), queryPage);
+        PagingBase<OrderEntity> pagingBase = orderService.listOrder(getUid(), queryPage);
         return new Result<PagingBase>().ok(pagingBase);
     }
 
     @PostMapping("/saveCommission")
     @ApiOperation("生成直播提成")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "day", value = "生成指定多少天前的一天数据",  dataType = "Integer", defaultValue = "null"),
+            @ApiImplicitParam(name = "day", value = "生成指定多少天前的一天数据", dataType = "Integer", defaultValue = "null"),
     })
     public Result saveCommission(Integer day) {
         int total = orderCommissionService.saveOrderCommission(day);
@@ -89,7 +83,7 @@ public class OrderController extends BaseController {
     @ApiOperation("行情数据")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "symbol", value = "交易对,默认btcusdt", required = true, dataType = "String", defaultValue = "btcusdt"),
-            @ApiImplicitParam(name = "interval", allowableValues = "1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d", value = "行情周期,默认1m",  dataType = "String", defaultValue = "1m"),
+            @ApiImplicitParam(name = "interval", allowableValues = "1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d", value = "行情周期,默认1m", dataType = "String", defaultValue = "1m"),
             @ApiImplicitParam(name = "size", value = "行情条数,默认240条", dataType = "int", defaultValue = "240"),
     })
     public Result<List<CandlestickDto>> getCandlestick(String symbol, String interval, Integer size) {
