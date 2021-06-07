21bba613fae63cb7de4a1754052b421beb793d06
frank <frank.han@cf688.net>
2021-06-07 11:45:20 +0800

    拦截器更改

--- cf-utils/src/main/java/com/cf/util/utils/Const.java
+++ cf-utils/src/main/java/com/cf/util/utils/Const.java
@@ -7,51 +7,14 @@ package com.cf.util.utils;
  */
 public class Const {
 
-	//cf
-	//mq-topic 存款和完善资料
-	public static  final  String MQ_TOPIC_TASK_CASHIN ="mq_topic_task_cashin";
+	public static  final  String PUBLIC ="/public/";
+	public static  final  String PUBLIC_URL ="/public/**";
 
-	//mq-topic 模拟交易
-	public static  final  String MQ_TOPIC_TASK_DEMO_TRADE ="mq_topic_task_demo_trade";
+	public static  final  String API ="/api/";
+	public static  final  String API_URL ="/api/**";
 
-	//mq-topic 模拟交易
-	public static  final  String MQ_TOPIC_TASK_REAL_TRADE ="mq_topic_task_real_trade";
-
-	//cf任务mongo表
-	public static  final  String MONGO_ACTIVE_TASK_CF ="cf_active_task";
-
-	//cfvn
-	//mq-topic 存款和完善资料
-	public static  final  String MQ_TOPIC_FVN_TASK_CASHIN ="mq_topic_cfvn_task_cashin";
-
-	//mq-topic 模拟交易
-	public static  final  String MQ_TOPIC_FVN_TASK_DEMO_TRADE ="mq_topic_cfvn_task_demo_trade";
-
-	//mq-topic 模拟交易
-	public static  final  String MQ_TOPIC_CFVN_TASK_REAL_TRADE ="mq_topic_cfvn_task_real_trade";
-
-
-	//cfvn任务mongo表（参与活动用户）
-	public static  final  String MONGO_ACTIVE_TASK_USER_CFVN ="cfvn_active_user_task";
-
-	public static  final  String CFVN_TRADE_USER ="cfvn_trade_user_";
-
-
-	//crs产品类型
-	public static final String CF_CRS_ZONE_TYPE = "cf:new:crs:zone:type";
-
-	//渠道
-	public static final String  CHANNEL_CF = "CFD";
-	public static final String  CHANNEL_CX = "CX";
-
-	//mq-topic 设备绑定账号
-	public static  final  String TOPIC_MIS_FAIDSTR = "topic_cf_mis_customer_faidstr";
-
-	//mq-topic 设备绑定账号
-	public static  final  String TOPIC_SYN_ACCOUNT_INFO = "topic_syn_account_info_";
-
-	//mq-topic 设备
-	public static  final  String TOPIC_SYN_ACCOUNT_INFO_16 = "topic_syn_account_info_16";
+	public static  final  String ADMIN ="/admin/";
+	public static  final  String ADMIN_URL ="/admin/**";
 
 
 
