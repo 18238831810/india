b83d6a90e7b4ad05d393af4f505ae6b9dee3d2f8
frank <frank.han@cf688.net>
2021-06-12 14:52:37 +0800

    跨越设置

--- crs-admin/src/main/java/com/cf/crs/config/config/CorsConfig.java
+++ crs-admin/src/main/java/com/cf/crs/config/config/CorsConfig.java
@@ -3,26 +3,33 @@ package com.cf.crs.config.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
- * @author leek
+ * @author frank
  * @description 跨域配置
  * @date 2019/8/13 16:06
  */
 @Configuration
 public class CorsConfig {
 
+    public CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setMaxAge(36000L);
+        return corsConfiguration;
+    }
+
     @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.setAllowCredentials(true);
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration("/**", config);
-        return new CorsFilter(configSource);
+    public CorsWebFilter corsFilter() {
+        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", buildConfig());
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
