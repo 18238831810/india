11109245d0ac9d38d37cc78861a31217c441d8ae
frank <frank.han@cf688.net>
2021-07-16 21:01:57 +0800

    注册充值推广奖励

--- crs-admin/src/main/java/com/cf/crs/entity/CashinRebateDto.java
+++ crs-admin/src/main/java/com/cf/crs/entity/CashinRebateDto.java
@@ -1,13 +1,8 @@
 package com.cf.crs.entity;
 
-import cn.afterturn.easypoi.excel.annotation.Excel;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.cf.crs.common.entity.QueryPage;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -25,29 +20,26 @@ public class CashinRebateDto extends QueryPage implements Serializable {
     /**
      * 用户ID
      */
-    @Excel(name = "用户账号")
     @ApiModelProperty(value = "用户ID")
     private Long uid;
 
-    @Excel(name = "直接推荐人")
     @ApiModelProperty(value = "直接推荐人")
     private Long direct;
 
-    @Excel(name = "间接推荐人")
     @ApiModelProperty(value = "间接推荐人")
     private Long indirect;
 
-    @Excel(name = "直接返利金额")
     @ApiModelProperty(value = "直接返利金额")
     private BigDecimal cashinDirect;
 
-    @Excel(name = "间接返利金额")
     @ApiModelProperty(value = "间接返利金额")
     private BigDecimal cashinIndirect;
 
-    @Excel(name = "返利时间")
     @ApiModelProperty(value = "返利时间")
     private Long createTime;
+
+    @ApiModelProperty(value = "返利类型（0:注册充值返利,1:交易返利）")
+    private Integer type = 0;
 
 
 }
